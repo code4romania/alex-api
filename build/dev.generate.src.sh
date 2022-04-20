778041a21f0c9ec574a34e820d0d704ea136a37f
Andrei Floroiu <floroiu.andrew@gmail.com>
2022-04-20 20:53:05 +0000

    Updated OpenAPI specs for balance sheet items. Updated generate script to target program and not library.

--- build/dev.generate.src.sh
+++ build/dev.generate.src.sh
@@ -14,4 +14,4 @@ fi
 # Validate, bundle and generate
 swagger-cli validate specs/alex-api.yml\
     && swagger-cli bundle specs/alex-api.yml --outfile specs/alex-api-bundle.yml --type yaml\
-    && openapi-generator-cli generate -i specs/alex-api-bundle.yml -o _build/aspnetcorestubs -g aspnetcore --additional-properties=aspnetCoreVersion=5.0,buildTarget=library,isLibrary=true,operationIsAsync=true,operationResultTask=true,useDefaultRouting=false
+    && openapi-generator-cli generate -i specs/alex-api-bundle.yml -o _build/aspnetcorestubs -g aspnetcore --additional-properties=aspnetCoreVersion=5.0,buildTarget=program,isLibrary=false,operationIsAsync=true,operationResultTask=true,useDefaultRouting=false
