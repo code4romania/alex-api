2594d2b5ffdfecfd07ca24f626c802089fb5296e
Andrei Floroiu <floroiu.andrew@gmail.com>
2022-04-21 19:22:57 +0000

    Reconfigured dev env to use swagger-codegen v3 for generating server stubs as openapi-generator does not support inheritance properly.

--- build/dev.generate.src.sh
+++ build/dev.generate.src.sh
@@ -10,8 +10,15 @@ if [ -f "specs/alex-api-bundle.yml" ]; then
     rm specs/alex-api-bundle.yml
     echo 'Deleted specs/alex-api-bundle.yml...'
 fi
+if [ -f "specs/alex-api-bundle.json" ]; then
+    rm specs/alex-api-bundle.json
+    echo 'Deleted specs/alex-api-bundle.json...'
+fi
 
 # Validate, bundle and generate
 swagger-cli validate specs/alex-api.yml\
-    && swagger-cli bundle specs/alex-api.yml --outfile specs/alex-api-bundle.yml --type yaml\
-    && openapi-generator-cli generate -i specs/alex-api-bundle.yml -o _build/aspnetcorestubs -g aspnetcore --additional-properties=aspnetCoreVersion=5.0,buildTarget=program,isLibrary=false,operationIsAsync=true,operationResultTask=true,useDefaultRouting=false
+    && swagger-cli bundle specs/alex-api.yml --outfile specs/alex-api-bundle.json --type json\
+    && java -jar /swagger-codegen-cli.jar generate -i specs/alex-api-bundle.json -l aspnetcore -o _build/swagger-codegen -c build/swagger-codegen.config
+# swagger-cli validate specs/alex-api.yml\
+#     && swagger-cli bundle specs/alex-api.yml --outfile specs/alex-api-bundle.yml --type yaml\
+#     && openapi-generator-cli generate -i specs/alex-api-bundle.yml -o _build/openapi-generator -g aspnetcore --additional-properties=aspnetCoreVersion=5.0,buildTarget=program,isLibrary=false,operationIsAsync=true,operationResultTask=true,useDefaultRouting=false
