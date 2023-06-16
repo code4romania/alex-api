e37f88a2b070b72335b181d3e1e9f6c7c462c1d3
Andrei Floroiu <floroiu.andrew@gmail.com>
2023-06-16 22:23:27 +0300

    Minor bash syntax fix

--- build/dev.generate.src.sh
+++ build/dev.generate.src.sh
@@ -12,6 +12,6 @@ if [ -f "specs/alex-api-bundle.yml" ]; then
 fi
 
 # Validate, bundle and generate
-swagger-cli validate specs/alex-api.yml\
-    && swagger-cli bundle specs/alex-api.yml --outfile specs/alex-api-bundle.yml --type yaml\
+swagger-cli validate specs/alex-api.yml \
+    && swagger-cli bundle specs/alex-api.yml --outfile specs/alex-api-bundle.yml --type yaml \
     && openapi-generator-cli generate -i specs/alex-api-bundle.yml -o _build/aspnetcorestubs -g aspnetcore --additional-properties=aspnetCoreVersion=5.0,buildTarget=library,isLibrary=true,operationIsAsync=true,operationResultTask=true,useDefaultRouting=false
